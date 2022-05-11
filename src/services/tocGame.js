d55b0869d89ba564991f183644ca856f4dd8a988
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2022-05-11 17:40:05 +0200

    Mesas

--- src/services/tocGame.js
+++ src/services/tocGame.js
@@ -4,7 +4,8 @@ import router from '../router/index';
 import { useToast } from "vue-toastification";
 const toast = useToast();
 
-const baseURL = 'http://localhost:3000/'; // BUILD PARA TPV
+// const baseURL = 'http://localhost:3000/'; // BUILD PARA TPV
+const baseURL = 'http://10.137.0.201:3000/'; // BUILD PARA TPV CON IP
 // const baseURL = 'http://54.195.159.7:3000'; // BUILD PARA SERVIDOR
 
 class tocGameV3 {
